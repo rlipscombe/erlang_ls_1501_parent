ecc66736bfd2399833c70bff9cdb06de116881f6
Roger Lipscombe <roger@differentpla.net>
2024-03-07 15:27:50 +0000

    Bootstrap

---
+++ Makefile
@@ -0,0 +1,5 @@
+PROJECT = erlang_ls_1501_parent
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.1.0
+
+include erlang.mk
---
+++ src/erlang_ls_1501_parent_app.erl
@@ -0,0 +1,11 @@
+-module(erlang_ls_1501_parent_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	erlang_ls_1501_parent_sup:start_link().
+
+stop(_State) ->
+	ok.
